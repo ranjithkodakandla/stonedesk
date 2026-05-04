903468594184b29b6c7210892ef512a34aa6d8f5
ranjithkodakandla <55225977+ranjithkodakandla@users.noreply.github.com>
2026-05-04 23:34:41 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -95,7 +95,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://stonedesk-wwrc.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
